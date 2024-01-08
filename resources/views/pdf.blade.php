8e200d969b491d57ed0f98a44f0b2dcfb8f42da4
Erfan Ebrahimi <persionhost@gmail.com>
2024-01-08 18:58:09 +0330

    remove name and logo from header pdf
    https://trello.com/c/Nr3uz16q/5-pdf-edits

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,19 +1,9 @@
 <table style="width: 90%;margin: 10px auto;">
     <tr>
-        <td colspan="2" style="border: none;text-align: center;padding-top: 20px;">
-            <strong>{{ \HackerESQ\Settings\Facades\Settings::get('site_title') }}</strong>
-        </td>
-    </tr>
-    <tr>
         <td style="width: 80%;border-right: none;border-top: none;vertical-align: middle;padding: 20px 20px;">
             <strong>Application Form Of  {{ $application->SFName }}</strong>
             <div style="margin-top:10px;"></div>
             <strong>Printed at</strong>: {{ now() }}
-        </td>
-        <td style="width: 20%;border-left: none;border-top: none; text-align: right;padding: 10px;">
-            @if(\HackerESQ\Settings\Facades\Settings::get('logo_dark'))
-            <img style="max-width: 100%;" src="{{ asset(Str::replaceFirst('public/' , 'storage/' , \HackerESQ\Settings\Facades\Settings::get('logo_dark'))) }}"/>
-            @endif
         </td>
     </tr>
 </table>
