4421d485f045855b1231c610f4a3eb07802f4815
Erfan Ebrahimi <persionhost@gmail.com>
2024-01-10 20:58:24 +0330

    add pdf chages
    https://trello.com/c/7aiirFsP/11-pdf-edits

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -3,8 +3,16 @@
     body { margin: 0; }
 </style>
 <body style="background-size:cover;background-repeat: no-repeat;background-image: url('{{ asset(Str::replaceFirst('public/' , 'storage/' , \HackerESQ\Settings\Facades\Settings::get('pdf_background'))) }}');">
-<div style="width: 100%;margin: 70px auto 50px;vertical-align: middle;text-align: center;">
-    <strong>Application Form  {{ $application->SFName }}</strong>
+<div style="width: 100%;margin: 70px auto 85px;vertical-align: middle;text-align: center;">
+    {{--    <strong>Application Form:</strong>  {{ $application->SFName }}--}}
+    {{--    <div style="margin-top:10px;"></div>--}}
+    {{--    <strong>Submission Date & Time:</strong>  {{ $application->created_at }}--}}
+</div>
+<div style="width: 220px;font-size:12px;line-height:5px;float:right;margin-right:47px;margin-top:-30px; background-color: #344050;color: white;padding: 8px 5px;">
+    <small style="">Submission Date & Time: {{ $application->created_at }}</small>
+</div>
+<div style="width:692px;margin: 0 auto 6px; background-color: #344050;color: white;padding: 5px 5px;">
+    <strong>Application Form: {{ $application->SFName }}</strong>
 </div>
 <div style="width: 90%;margin: 0 auto;">
     @include('report' , ['application' => $application])
